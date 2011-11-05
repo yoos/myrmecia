1bec71c81f669f9ef817d6274024bc3044cb4048
yoos <yoos117@gmail.com>
2011-11-05 15:40:57 -0700

    Not the cleanest or proper code, but ants actually move away from the hive in a non-zombielike way!

--- misc.lisp
+++ misc.lisp
@@ -6,3 +6,8 @@
     (nth (random (length some-list)) some-list)
     (error "Argument to get-random-elt not a list or list is empty.")))
 
+(defun get-directions (a-row a-col t-row t-col)
+  (append (if (> t-row a-row) '(:north) '(:south))
+          (if (> t-col a-col) '(:east) '(:west))))
+
+
